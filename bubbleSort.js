b5ecbe624b4403aa5484aee7467c9a6bb24b6388
ctsygiel <ctsygiel@gmail.com>
2017-04-03 19:53:05 -1000

    modularized and cleaned up

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,27 +1,35 @@
 /*jshint esversion: 6*/
-module.exports = (numbers) => {
+module.exports = () => {
 
-var swapped;
-var temp;
-//Loops through numbers, defaults "swapped" to false.
-for (var i = 0; i < numbers.length; i++){
-  swapped = false;
+  function mrSortyPants(numbers) {
+    var swapped;
+    var temp;
 
-  //Loops through numbers and swaps numbers, sets "swapped" to true.
-  for (var y = 0; y < numbers.length; y++){
-    if(numbers[y] > numbers[y +1]){
-      temp = numbers[y];
-      numbers[y] = numbers[y + 1];
-      numbers[y + 1] = temp;
-      swapped = true;
+    //loops through numbers, defaults "swapped" to false.
+    for (var i = 0; i < numbers.length; i++){
+      swapped = false;
+
+      //loops through numbers and swaps numbers, sets "swapped" to true.
+      for (var y = 0; y < numbers.length; y++){
+        if(numbers[y] > numbers[y +1]){
+          temp = numbers[y];
+          numbers[y] = numbers[y + 1];
+          numbers[y + 1] = temp;
+          swapped = true;
+        }
+      }
+
+      //breaks the loop if nothing is left to be swapped
+      if(swapped === false){
+        break;
+      }
     }
+    return numbers;
   }
 
-  //breaks the loop if nothing is left to be swapped
-  if(swapped === false){
-    break;
-  }
-}
-return numbers;
+  return {
+    mrSortyPants: mrSortyPants
+  };
+
 };
 
--- tests/bubble-sort.spec.js
+++ tests/bubble-sort.spec.js
@@ -7,14 +7,12 @@ const bubbleSort = require('../bubbleSort.js');
 describe('sorting', () => {
 
   it('should return an array', () => {
-      expect(bubbleSort([1,2,3])).to.be.a('array');
+      expect(bubbleSort([1,2,3])).to.be.a('object');
     });
 
   it('should sort an unsorted array', () => {
-      expect(bubbleSort([5,4,3,2,1])).to.deep.equal([1,2,3,4,5]);
-      expect(bubbleSort([5,4,3,-2,1])).to.deep.equal([-2,1,3,4,5]);
+      expect(bubbleSort().mrSortyPants([5,4,3,2,1])).to.deep.equal([1,2,3,4,5]);
+      expect(bubbleSort().mrSortyPants([5,4,3,-2,1])).to.deep.equal([-2,1,3,4,5]);
     });
 
 });
-
-
